fb83024cb79410a6b509a701e15d249b44ad17e8
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-10-22 17:48:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -34,7 +34,7 @@ end
 
 def retrieve_first_element_from_array(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]\
-  array = array[0]
+  return array[0]
 end
 
 def retrieve_last_element_from_array(array)
